c512e6e84353646d86d4c80d114fa546245b24b8
Alex Marenin <great_muchacho@mail.ru>
2013-10-22 00:02:04 +0400

    Added some incomplete code

--- server.js
+++ server.js
@@ -2,4 +2,17 @@
  * @author Marenin Alex
  * October 2013
  */
- 
+
+
+var util = require( './util' ),
+    commander = require( 'commander' ),
+    express = require( 'express' );
+
+
+commander
+    .usage( '[options]' )
+    .option( '-p, --port [port]', 'Set Web server port', 1337 )
+    .option( '-c, --config [name]', 'Set the config name to use, default is "dev"', 'dev' )
+    .parse( process.argv );
+
+var config = util.getConfig( commander.config );
--- website/js/main.js
+++ website/js/main.js
@@ -4,31 +4,44 @@
  */
 
 
-$( function(){
-
-
-
-});
-
-
-
-
-
-
-
-
-
-
-
 function Chat( options ){
     this.remoteVideo = options.remoteVideo;
     this.localVideo = options.localVideo;
     this.socketUrl = options.socketUrl;
+    this.id = String( Date.now() + Math.random() );
 }
 
 
-Chat.prototype.start = function( callback ){
+Chat.MEDIA_AUDIO = 1;
+Chat.MEDIA_VIDEO = 2;
+Chat.MEDIA_SCREEN = 3;
+
+
+Chat.prototype.connect = function(){
     this.socket = io.connect( this.socketUrl );
-    this.socket.on( 'connect', function(){} );
+    this.socket.emit( 'initUser', {userId: this.id} );
 };
 
+
+Chat.prototype.joinRoom = function( options, callback ){
+    var chat = this;
+    this.socket.emit( 'joinRoom', {
+        roomName: options.roomName,
+        password: options.password || '',
+        mediaType: options.media || Chat.MEDIA_VIDEO
+    }, function( res ){
+        if ( res.error )
+            callback( res.error );
+        else {
+            chat.roomName = res.roomName;
+            chat.members = res.members;
+            chat.connectWithMembers();
+            callback();
+        }
+    });
+};
+
+
+Chat.prototype.connectWithMembers = function(){
+
+};
